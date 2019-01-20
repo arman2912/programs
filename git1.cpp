e040506f6081e50a76211e8e746e7f7230e61592
arman <armankaur915@gmail.com>
2019-01-20 20:44:08 +0530

    added second code

---
+++ git1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"my name is arman"<<endl<<"my adress:chandigarh";
+	return 0;
+}
---
+++ git2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int d;
+	cin>>d;
+	d=d*220;
+	cout<<"distance in yards="<<d;
+	return 0;
+}
